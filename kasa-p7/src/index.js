79e6c86e1cf704ee01a6af1da1e16999b772a1a3
Kabla06 <hcalviera@gmail.com>
2022-11-06 21:08:27 +0100

    petit setup au calme

--- kasa-p7/src/index.js
+++ kasa-p7/src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import Home from "./pages/Home";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Accueil from './pages/AccueilApp'
+import { BrowserRouter as Router} from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Accueil />
+    </Router>
   </React.StrictMode>
-);
+)
---
+++ kasa-p7/src/pages/AccueilApp.jsx
@@ -0,0 +1,12 @@
+import Fiche from "../components/FicheLogement";
+
+function Accueil() {
+
+  return (
+    <div>
+      <Fiche />
+    </div>
+  );
+}
+
+export default Accueil
